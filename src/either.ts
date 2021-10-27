a91d6b99c3e3c024aa072f214c1f2bcb6726ab6e
Maciej Miklas <mac.miklas@gmail.com>
2021-10-27 07:44:01 +0200

    dev

--- src/either.ts
+++ src/either.ts
@@ -1,205 +1,213 @@
 import * as R from 'ramda';
 
 export abstract class Either<T> {
-	protected readonly val: T;
-	protected readonly msg: string;
+    protected readonly val: T;
+    protected readonly msg: string;
+    protected static logEnabled = false;
 
-	protected constructor(value: T, msg: string) {
-		this.val = value;
-		this.msg = msg;
-	}
+    protected constructor(value: T, msg: string) {
+        this.val = value;
+        this.msg = msg;
+    }
 
-	static until = <T>(next: (previous: T) => Either<T>, init: Either<T>, max: number = 500): Either<T[]> => {
-		let val = init;
-		const all: T[] = [];
-		let cnt = 0;
-		while (val.isRight()) {
-			if (cnt > max) {				
-				break;
-			}
-			val.exec((v) => all.push(v));
-			val = next(val.get());
-			cnt = cnt + 1;
-		}
-		return Either.ofCondition(() => all.length > 0, () => 'Empty result for ' + init, () => all);
-	};
+    static setLog(enabled: boolean): void {
+        Either.logEnabled = enabled;
+    }
 
-	static ofArray = <T>(...args: Either<T>[]): Either<T[]> => {
-		const ret = args.filter(e => e.isRight()).map(e => e.get());
-		return Either.ofCondition(() => ret.length > 0, () => 'All candidates for Array are Left: ' + args, () => ret);
-	};
+    static until = <T>(next: (previous: T) => Either<T>, init: Either<T>, max: number = 500): Either<T[]> => {
+        let val = init;
+        const all: T[] = [];
+        let cnt = 0;
+        while (val.isRight()) {
+            if (cnt > max) {
+                break;
+            }
+            val.exec((v) => all.push(v));
+            val = next(val.get());
+            cnt = cnt + 1;
+        }
+        return Either.ofCondition(() => all.length > 0, () => 'Empty result for ' + init, () => all);
+    };
 
-	static ofRight<T>(val: T): Either<T> {
-		return new Right<T>(val);
-	}
+    static ofArray = <T>(...args: Either<T>[]): Either<T[]> => {
+        const ret = args.filter(e => e.isRight()).map(e => e.get());
+        return Either.ofCondition(() => ret.length > 0, () => 'All candidates for Array are Left: ' + args, () => ret);
+    };
 
-	static ofLeft<T>(msg: string): Either<T> {
-		return new Left(msg);
-	}
+    static ofRight<T>(val: T): Either<T> {
+        return new Right<T>(val);
+    }
 
-	static ofNullable<T>(val: T | undefined, msg: () => string): Either<T> {
-		return R.isNil(val) ? new Left<T>(msg()) : new Right<T>(val);
-	}
+    static ofLeft<T>(msg: string): Either<T> {
+        return new Left(msg);
+    }
 
-	static ofCondition<T>(cnd: () => boolean, left: () => string, right: () => T): Either<T> {
-		return cnd() ? new Right<T>(right()) : new Left<T>(left());
-	}
+    static ofNullable<T>(val: T | undefined, msg: () => string): Either<T> {
+        return R.isNil(val) ? new Left<T>(msg()) : new Right<T>(val);
+    }
 
-	static ofTruth<T>(truth: Either<any>[], right: () => T): Either<T> {
-		const left = truth.filter(e => e.isLeft());
-		return left.length === 0 ? new Right<T>(right()) : new Left(left.map(l => l.message()).join(','));
-	}
+    static ofCondition<T>(cnd: () => boolean, left: () => string, right: () => T): Either<T> {
+        return cnd() ? new Right<T>(right()) : new Left<T>(left());
+    }
 
-	get(): T {
-		return this.val;
-	}
+    static ofTruth<T>(truth: Either<any>[], right: () => T): Either<T> {
+        const left = truth.filter(e => e.isLeft());
+        return left.length === 0 ? new Right<T>(right()) : new Left(left.map(l => l.message()).join(','));
+    }
 
-	message(): string {
-		return this.msg;
-	}
+    get(): T {
+        return this.val;
+    }
 
-	abstract mapGet<V>(left: (msg: string) => V, right: (v: T) => V): V;
+    message(): string {
+        return this.msg;
+    }
 
-	abstract orElse(other: T): T;
+    abstract mapGet<V>(left: (msg: string) => V, right: (v: T) => V): V;
 
-	abstract orElseGet(fn: () => T): T;
+    abstract orElse(other: T): T;
 
-	abstract isLeft(): boolean;
+    abstract orElseGet(fn: () => T): T;
 
-	abstract isRight(): boolean;
+    abstract isLeft(): boolean;
 
-	abstract map(fn: (val: T) => any): Either<any>;
+    abstract isRight(): boolean;
 
-	abstract remap<V>(mf: (val: T) => Either<V>, jf: (v: V, t: T) => void): Either<V>;
+    abstract map(fn: (val: T) => any): Either<any>;
 
-	abstract append<V>(producer: (val: T) => Either<V>, appender: (t: T, v: V) => void): Either<T>;
+    abstract remap<V>(mf: (val: T) => Either<V>, jf: (v: V, t: T) => void): Either<V>;
 
-	abstract exec(fn: (v: T) => void): Either<T>;
+    abstract append<V>(producer: (val: T) => Either<V>, appender: (t: T, v: V) => void): Either<T>;
 
-	abstract assert(fn: (v: T) => Either<string>): Either<T>;
+    abstract exec(fn: (v: T) => void): Either<T>;
+
+    abstract assert(fn: (v: T) => Either<string>): Either<T>;
 }
 
 export class Left<T> extends Either<T> {
 
-	constructor(msg: string) {
-		super(null as any, msg);
-	}
+    constructor(msg: string) {
+        super(null as any, msg);
+        if (Either.logEnabled) {
+            console.log('EI-L> ' + msg);
+        }
+    }
 
-	mapGet<V>(left: (msg: string) => V, right: (v: T) => V): V {
-		return left(this.msg);
-	}
+    mapGet<V>(left: (msg: string) => V, right: (v: T) => V): V {
+        return left(this.msg);
+    }
 
-	assert(fn: (v: T) => Either<string>): Either<T> {
-		return this;
-	}
+    assert(fn: (v: T) => Either<string>): Either<T> {
+        return this;
+    }
 
-	map(fn: (val: T) => any): Either<any> {
-		return this;
-	}
+    map(fn: (val: T) => any): Either<any> {
+        return this;
+    }
 
-	remap<V>(mf: (val: T) => Either<V>, jf: (v: V, t: T) => void): Either<V> {
-		return this as unknown as Either<V>;
-	}
+    remap<V>(mf: (val: T) => Either<V>, jf: (v: V, t: T) => void): Either<V> {
+        return this as unknown as Either<V>;
+    }
 
-	exec(fn: (v: T) => void): Either<T> {
-		return this;
-	}
+    exec(fn: (v: T) => void): Either<T> {
+        return this;
+    }
 
-	isLeft(): boolean {
-		return true;
-	}
+    isLeft(): boolean {
+        return true;
+    }
 
-	append<V>(producer: (v: T) => Either<V>, appender: (t: T, v: V) => void): Either<T> {
-		return this;
-	}
+    append<V>(producer: (v: T) => Either<V>, appender: (t: T, v: V) => void): Either<T> {
+        return this;
+    }
 
-	isRight(): boolean {
-		return false;
-	}
+    isRight(): boolean {
+        return false;
+    }
 
-	orElse(other: T): T {
-		return other;
-	}
+    orElse(other: T): T {
+        return other;
+    }
 
-	orElseGet(fn: () => T): T {
-		return fn();
-	}
+    orElseGet(fn: () => T): T {
+        return fn();
+    }
 
-	get(): T {
-		throw new TypeError('Left has no value: ' + this.message());
-	}
+    get(): T {
+        throw new TypeError('Left has no value: ' + this.message());
+    }
 
-	toString(): string {
-		return `Left[${this.message()}]`;
-	}
+    toString(): string {
+        return `Left[${this.message()}]`;
+    }
 }
 
 export class Right<T> extends Either<T> {
 
-	constructor(value: T) {
-		super(value, 'Right');
-	}
+    constructor(value: T) {
+        super(value, 'Right');
+    }
 
-	assert(fn: (v: T) => Either<string>): Either<T> {
-		const resp = fn(this.val);
-		if (resp.isLeft()) {			
-			return Either.ofLeft(resp.message());
-		}
-		return this;
-	}
+    assert(fn: (v: T) => Either<string>): Either<T> {
+        const resp = fn(this.val);
+        if (resp.isLeft()) {
+            return Either.ofLeft(resp.message());
+        }
+        return this;
+    }
 
-	orElseGet(fn: () => T): T {
-		return this.val;
-	}
+    orElseGet(fn: () => T): T {
+        return this.val;
+    }
 
-	exec(fn: (v: T) => void): Either<T> {
-		fn(this.get());
-		return this;
-	}
+    exec(fn: (v: T) => void): Either<T> {
+        fn(this.get());
+        return this;
+    }
 
-	append<V>(producer: (v: T) => Either<V>, appender: (t: T, v: V) => void): Either<T> {
-		const val = this.get();
-		const res = producer(val);
-		if (R.isNil(res) || res.isLeft()) {
-			return Either.ofLeft('append got null for ' + val);
-		}
-		appender(val, res.get());
-		return new Right(val);
-	}
+    append<V>(producer: (v: T) => Either<V>, appender: (t: T, v: V) => void): Either<T> {
+        const val = this.get();
+        const res = producer(val);
+        if (R.isNil(res) || res.isLeft()) {
+            return Either.ofLeft('append got null for ' + val);
+        }
+        appender(val, res.get());
+        return new Right(val);
+    }
 
-	map(fn: (v: T) => any): Either<any> {
-		const val = this.get();
-		const res = fn(val);
-		return R.isNil(res) ? Either.ofLeft('map got null for ' + val) : res instanceof Either ? res : new Right(res);
-	}
+    map(fn: (v: T) => any): Either<any> {
+        const val = this.get();
+        const res = fn(val);
+        return R.isNil(res) ? Either.ofLeft('map got null for ' + val) : res instanceof Either ? res : new Right(res);
+    }
 
-	remap<V>(mf: (val: T) => Either<V>, jf: (v: V, t: T) => void): Either<V> {
-		const val = this.get();
-		const res = mf(val);
-		if (R.isNil(res) || res.isLeft()) {
-			return Either.ofLeft('remap got null for ' + val);
-		}
-		jf(res.get(), val);
-		return res;
-	}
+    remap<V>(mf: (val: T) => Either<V>, jf: (v: V, t: T) => void): Either<V> {
+        const val = this.get();
+        const res = mf(val);
+        if (R.isNil(res) || res.isLeft()) {
+            return Either.ofLeft('remap got null for ' + val);
+        }
+        jf(res.get(), val);
+        return res;
+    }
 
-	isLeft(): boolean {
-		return false;
-	}
+    isLeft(): boolean {
+        return false;
+    }
 
-	isRight(): boolean {
-		return true;
-	}
+    isRight(): boolean {
+        return true;
+    }
 
-	orElse(other: T): T {
-		return R.isNil(this.val) ? other : this.val;
-	}
+    orElse(other: T): T {
+        return R.isNil(this.val) ? other : this.val;
+    }
 
-	toString(): string {
-		return `Right[${this.get()}]`;
-	}
+    toString(): string {
+        return `Right[${this.get()}]`;
+    }
 
-	mapGet<V>(left: (msg: string) => V, right: (v: T) => V): V {
-		return right(this.val);
-	}
+    mapGet<V>(left: (msg: string) => V, right: (v: T) => V): V {
+        return right(this.val);
+    }
 }
